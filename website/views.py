1bc070f736cee5eb182645fd934ab4dc50e34a31
BananaSully <mitchell_0710@outlook.com>
2022-11-01 15:49:57 +1000

    Updated

--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, request, redirect,url_for
-from .models import Destination 
+from .models import Events 
 
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
 def index():
-    destinations = Destination.query.all()    
+    destinations = Events.query.all()    
     return render_template('index.html', destinations=destinations)
 
 @mainbp.route('/search')
@@ -13,7 +13,7 @@ def search():
     if request.args['search']:
         print(request.args['search'])
         dest = "%" + request.args['search'] + '%'
-        destinations = Destination.query.filter(Destination.description.like(dest)).all()
+        destinations = Events.query.filter(Events.description.like(dest)).all()
         return render_template('index.html', destinations=destinations)
     else:
         return redirect(url_for('main.index'))
