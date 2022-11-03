aca3d11bb14faa1abea33dd721a2853fdc60e5e5
Your Name <tonyliyumo@gmail.com>
2022-11-03 18:27:51 +1000

    update genre

--- website/views.py
+++ website/views.py
@@ -19,15 +19,6 @@ def Options():
     else:
         return redirect(url_for('main.index'))
     
-@mainbp.route('/Options')
-def Options():
-    if request.args['Options']:
-        print(request.args['Options'])
-        dest = "%" + request.args['Options'] + '%'
-        destinations = Destination.query.filter(Destination.genre.like(dest)).all()
-        return render_template('index.html', destinations=destinations)
-    else:
-        return redirect(url_for('main.index'))
     
 @mainbp.route('/search')
 def search():
