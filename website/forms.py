986488c9daeb9ff663439ddfb230a6e09c705f56
dojafish1 <cyberninjaboss@gmail.com>
2022-11-03 21:28:47 +1000

    added

--- website/forms.py
+++ website/forms.py
@@ -1,15 +1,36 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms.fields import StringField,TextAreaField,SubmitField,PasswordField,DateField,SelectField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
+from wtforms.validators import (DataRequired,Email,EqualTo,Length,URL)
 
 ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 
-#Create new destination
+#Create new event
 class DestinationForm(FlaskForm):
   name = StringField('Event Name', validators=[InputRequired()])
   venue = StringField('Venue', validators=[InputRequired()])
-  genre = StringField('Music Genre', validators=[InputRequired()])
+  genre = SelectField('Music Genre',[DataRequired()],choices=[('Rock'),('Pop'),('Jazz'),('Hip-Pop'),('Metal'),('Classical'),('EDM')])
+  #genre = StringField('Music Genre', validators=[InputRequired()])
+  ticketPrice = StringField('Ticket Price', validators=[InputRequired()])
+  overview = TextAreaField('Overview', 
+  validators=[InputRequired()])
+  description = TextAreaField('Description', 
+  validators=[InputRequired()])
+  status = SelectField('Status',[DataRequired()],choices=[('Upcoming'),('Open'),('Sold-out'),('Cancelled'),('Active'),])
+  #status = StringField('Status', validators=[InputRequired()])
+  image = FileField('Upload Cover Image', validators=[
+  FileRequired(message='Image cannot be empty'),
+  FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+  
+  
+  
+  submit = SubmitField("Create")
+
+
+class Tickets(FlaskForm):
+  number = StringField('Venue', validators=[InputRequired()])
+  total = StringField('Music Genre', validators=[InputRequired()])
   ticketPrice = StringField('Ticket Price', validators=[InputRequired()])
   overview = TextAreaField('Overview', 
   validators=[InputRequired()])
@@ -19,8 +40,8 @@ class DestinationForm(FlaskForm):
   image = FileField('Upload Cover Image', validators=[
   FileRequired(message='Image cannot be empty'),
   FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
-  
-  submit = SubmitField("Create")
+
+submit = SubmitField("Create")
     
 #User login
 class LoginForm(FlaskForm):
